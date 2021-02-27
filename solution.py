84a700162bd7157ceb9be30761ab8a8cfd93bf01
Jason Rivera <jar10045@nyu.edu>
2021-02-27 09:03:07 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -9,11 +9,11 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    port = '1025'
    mailserver = '127.0.0.1'
    # Create socket called clientSocket and establish a TCP connection with mailserver and port
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.bind ((mailserver, 1025))
-clientSocket.connect(mailserver)
-clientSocket.listen(1)
-connectionSocket, addr = clientSocket.accept()
+   clientSocket = socket(AF_INET, SOCK_STREAM)
+   clientSocket.bind ((mailserver, 1025))
+   clientSocket.connect(mailserver)
+   clientSocket.listen(1)
+   connectionSocket, addr = clientSocket.accept()
 
 
 
@@ -30,7 +30,7 @@ if recv[:3] != '220':
    clientSocket.send(heloCommand.encode())
    recv1 = clientSocket.recv(1024).decode()
    #print(recv1)
-   if recv1[:3] != '250':
+if recv1[:3] != '250':
    # print('250 reply not received from server.')
    pass
    # Send MAIL FROM command and print server response.
@@ -38,20 +38,20 @@ if recv[:3] != '220':
    clientSocket.send(mailFrom.encode())
    recv2 = clientSocket.recv(1024)
    #print("After MAIL FROM command: "+recv2)
-   if recv1[:3] != '250':
+if recv1[:3] != '250':
    pass
    # Send RCPT TO command and print server response.
    rcpTo= "RCPT TO: <jar10045@nyu.edu> \r\n"
    clientSocket.send(rcptTo.encode())
    recv3 = clientSocket.recv(1024)
    #print("After RCPT TO command: "+recv3)
-   if recv1[:3] != '250':
+if recv1[:3] != '250':
    pass
 # Send DATA command and print server response.
-data = "DATA\r\n"
-clientSocket.send(data.encode())
-recv4 = clientSocket.recv(1024)
-#print("After DATA command: " + recv4)
+   data = "DATA\r\n"
+   clientSocket.send(data.encode())
+   recv4 = clientSocket.recv(1024)
+   #print("After DATA command: " + recv4)
 if recv1[:3] != '250':
    pass
    # Send message data.
@@ -62,7 +62,7 @@ if recv1[:3] != '250':
    clientSocket.send(endmsg.encode())
    recv_msg = clientSocket.recv(1024)
    #print("Response after sending message body:" + recv_msg.decode())
-   if recv1[:3] != '250':
+if recv1[:3] != '250':
    pass
    # Message ends with a single period.
    # Fill in start
