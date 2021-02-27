307381662e5f1bb7f334006ef5258f0424d2eadb
Jason Rivera <jar10045@nyu.edu>
2021-02-27 09:28:04 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,13 +1,12 @@
 from socket import *
 
 
-def smtp_client(port=1025, mailserver='127.0.0.1'):
+def smtp_client(port='1025', mailserver='127.0.0.1'):
    msg = "\r\n My message"
    endmsg = "\r\n.\r\n"
 
    # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-   port = '1025'
-   mailserver = '127.0.0.1'
+
    # Create socket called clientSocket and establish a TCP connection with mailserver and port
    clientSocket = socket(AF_INET, SOCK_STREAM)
    clientSocket.bind ((mailserver, 1025))
