3d1d060c68e5b1ef695908599df7a09c95e92a25
Jason Rivera <jar10045@nyu.edu>
2021-02-28 17:52:14 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,20 +1,18 @@
 from socket import *
 
 
-def smtp_client(port=1025, mailserver='127.0.0.1'):
+def smtp_client(port=1025, mailserver='smtp.gmail.com'):
    msg = "\r\n My message"
    endmsg = "\r\n.\r\n"
 
    # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-   mailserver='smtp.nyu.edu'
-   port=25
+   #mailserver='smtp.gmail.com'
+   #port=25
 
    # Create socket called clientSocket and establish a TCP connection with mailserver and port
    clientSocket = socket(AF_INET, SOCK_STREAM)
-   clientSocket.bind((mailserver, port))
    clientSocket.connect((mailserver, port))
-   clientSocket.listen(1)
-   connectionSocket.addr = clientSocket.accept()
+   #connectionSocket.addr = clientSocket.accept()
 
 
    recv = clientSocket.recv(1024).decode()
@@ -26,7 +24,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    heloCommand = 'HELO Alice\r\n'
    clientSocket.send(heloCommand.encode())
    recv1 = clientSocket.recv(1024).decode()
-
+   if recv[:3] != '250':
+      pass
 
 
 
@@ -34,45 +33,44 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    # Send MAIL FROM command and print server response.
    mailFrom = "MAIL FROM: <jason.a.rivera@gmail.com> \r\n"
    clientSocket.send(mailFrom.encode())
-   recv2 = clientSocket.recv(1024)
+   recv2 = clientSocket.recv(1024).decode()
    if recv[:3] != '250':
       pass
 
 
    # Send RCPT TO command and print server response.
-   rcpTo= "RCPT TO: <jar10045@nyu.edu> \r\n"
+   rcptTo= "RCPT TO: <jason.a.rivera@gmail.com> \r\n"
    clientSocket.send(rcptTo.encode())
-   recv3 = clientSocket.recv(1024)
+   recv3 = clientSocket.recv(1024).decode()
    if recv[:3] != '250':
       pass
 
    # Send DATA command and print server response.
    data = "DATA\r\n"
    clientSocket.send(data.encode())
-   recv4 = clientSocket.recv(1024)
+   recv4 = clientSocket.recv(1024).decode()
    if recv[:3] != '250':
       pass
    # Send message data.
    subject = "Subject: SMTP mail client testing \r\n\r\n"
    clientSocket.send(subject.encode())
-   message = raw_input("Enter your message: \r\n")
-   clientSocket.send(message.encode())
+   clientSocket.send(msg.encode())
    clientSocket.send(endmsg.encode())
-   recv_msg = clientSocket.recv(1024)
+   recv_msg = clientSocket.recv(1024).decode()
 
    if recv[:3] != '220':
       pass
    # Message ends with a single period.
    endmsg = "\r\n.\r\n"
-   clientSocket.send(message + endmsg)
+   clientSocket.send(msg + endmsg)
    if recv1[:3] !='250':
       pass
    # Send QUIT command and get server response.
    clientSocket.send("QUIT\r\n".encode())
-   message = clientSocket.recv(1024)
+   message = clientSocket.recv(1024).decode()
    clientSocket.close()
 
 
 
 if __name__ == '__main__':
-   smtp_client(1025, '127.0.0.1')
+   smtp_client(1025, 'smtp.gmail.com')
